1718d24f19f75f6ba481f7574cb0bda62bf54f95
RocketRoss <code@radonn.co.za>
2020-08-29 14:02:06 +0200

    Sol: deposit and loan totals for 1 sheet

--- OptiNum.m
+++ OptiNum.m
@@ -1,3 +1,9 @@
 filepath = "D:\Development\Code\Scripts\MATLAB\OptiNumDatasets\BA900-2020-01-01csv\34118.csv"
 
 sheet = BA900Sheet(filepath)
+
+% totals = sheet.subtables(1).table(:, '"TOTAL(7)"')
+% depositTotal = totals('"1"', 1)
+depositTotal = sheet.subtables(1).table{'"1"', '"TOTAL(7)"'}
+subtable = getSubtableWithItemNumber(sheet, 110);
+loanTotal = subtable{'"110"', '"TOTAL ASSETS (Col 1 plus col 3)(5)"'}
