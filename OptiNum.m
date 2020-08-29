f74e762762c80b11c209fc9da6f56f64d222d44e
RocketRoss <code@radonn.co.za>
2020-08-29 14:08:29 +0200

    Sol: str2double results.

--- OptiNum.m
+++ OptiNum.m
@@ -4,6 +4,7 @@ sheet = BA900Sheet(filepath)
 
 % totals = sheet.subtables(1).table(:, '"TOTAL(7)"')
 % depositTotal = totals('"1"', 1)
-depositTotal = sheet.subtables(1).table{'"1"', '"TOTAL(7)"'}
+depositTotal = cellfun(@str2double, sheet.subtables(1).table{'"1"', '"TOTAL(7)"'})
+
 subtable = getSubtableWithItemNumber(sheet, 110);
-loanTotal = subtable{'"110"', '"TOTAL ASSETS (Col 1 plus col 3)(5)"'}
+loanTotal = cellfun(@str2double, subtable{'"110"', '"TOTAL ASSETS (Col 1 plus col 3)(5)"'})
